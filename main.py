fffa14a727d68976429cb7465785d4700444ef0f
FilipKDev <filip.kwasniewski@gmail.com>
2025-01-26 19:48:07 +0000

    added player to updatable and drawable group, implemented asteroid and asteroid field code to generate asteroids in game

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
 import pygame
-import player
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
     dt = 0
-    player_instance = player.Player((SCREEN_WIDTH / 2),(SCREEN_HEIGHT / 2))
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
+    player = Player((SCREEN_WIDTH / 2),(SCREEN_HEIGHT / 2))
+    asteroid_field = AsteroidField()
+
+    #print("Starting asteroids!")
+    #print(f"Screen width: {SCREEN_WIDTH}")
+    #print(f"Screen height: {SCREEN_HEIGHT}")
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player_instance.draw(screen)
-        player_instance.update(dt)
+        for obj in updatable:
+            obj.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
